0dacab8fb1e537408a326033725c8b2e155b7aa4
shwetapurushe <shwetapurushe@gmail.com>
2018-03-29 20:40:22 -0400

    Image carousel added in abouts section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ICarouselConfig, AnimationConfig } from 'angular4-carousel';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -8,6 +10,45 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
+
+  public imageSources: string[] = [
+      './assets/Image0.jpg',
+      './assets/Image1.jpg',
+      './assets/Image2.jpg',
+      './assets/Image3.jpg',
+      './assets/Image4.jpg',
+      './assets/Image5.jpg',
+      './assets/Image6.jpg',
+      './assets/Image7.jpg',
+      './assets/Image8.jpg',
+      './assets/Image9.jpg',
+      './assets/Image10.jpg',
+      './assets/Image11.jpg',
+      './assets/Image12.jpg',
+      './assets/Image13.jpg',
+      './assets/Image14.jpg',
+      './assets/Image15.jpg',
+      './assets/Image16.jpg',
+      './assets/Image17.jpg',
+      './assets/Image18.jpg',
+      './assets/Image19.jpg',
+      './assets/Image20.jpg',
+      './assets/Image21.jpg',
+      './assets/Image22.jpg',
+      './assets/Image23.jpg',
+      './assets/Image24.jpg',
+      './assets/Image25.jpg'
+  ];
+
+  public config: ICarouselConfig = {
+    verifyBeforeLoad: true,
+    log: false,
+    animation: true,
+    animationType: AnimationConfig.SLIDE,
+    autoplay: true,
+    autoplayDelay: 3000,
+    stopAutoplayMinWidth: 768
+  };
 
   ngOnInit() {
   }
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 
+import { CarouselModule } from 'angular4-carousel';
+
 @NgModule({
   imports: [
     CommonModule
+    ,CarouselModule
   ],
   declarations: [AboutComponent]
 })
