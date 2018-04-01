9ca43585f48ae9445b3bf9a5f3f09e647ab4bf9e
shwetapurushe <shwetapurushe@gmail.com>
2018-03-31 21:11:04 -0400

    conditional text description added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private description:string = "";
+
+  mouseEnter(div : string){
+
+      if(div == 'reco')
+        this.description = "I am fascinated by the origin and maintenance of biological diversity on" +
+"our planet. To explore this, I study the pretty ancient lineage–spiders, in particular the" +
+"orbicularians, using advanced phylogenomics technology combined with resultant morphology" +
+"to reconstruct the tree of life and trace the evolution of its structures.Trust me, it is exciting!"
+
+      else
+        this.description= "Knowledge about biodiversity does not seep easily from research papers to" +
+"general public in the age when biodiversity conservation is priority. To alleviate this knowledge" +
+"gap, I use social networking platforms to document spiders, starting in India [example: Fb group-" +
+"SpiderIndia (more than 7,000 members)], conduct meetings and bioblitz each year."
+
+   }
+
+   mouseLeave(div : string){
+      console.log('mouse leave :' + div);
+      this.description = "";
+
+      console.log("Leaving " +this.description);
+   }
+
+
   constructor() { }
 
   ngOnInit() {
